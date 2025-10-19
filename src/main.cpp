19e92c4787cf2e18a8353d24eda808d19b40d37e
gustavooarantes <profissional.gustavoa@gmail.com>
2025-10-19 18:22:51 -0300

    [feat]#02 - Initial implementation of classes and enums in the main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,73 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdint>
+#include <ctime>
+#include <format>
 #include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <variant>
+#include <vector>
 
-int main() {
-  std::cout << "Hello, world!" << std::endl;
-  return 0;
-}
+enum class OrderType { GoodTillCancel, FillAndKill };
+
+enum class Side { Buy, Sell };
+
+using Price = std::int32_t;
+using Quantity = std::uint32_t;
+using OrderId = std::uint64_t;
+
+struct LevelInfo {
+  Price price;
+  Quantity quantity;
+};
+
+using LevelInfos = std::vector<LevelInfo>;
+
+class OrderbookLevelInfos {
+public:
+  OrderbookLevelInfos(const LevelInfos &bids, const LevelInfos &asks)
+      : bids_{bids}, asks_{asks} {}
+
+  const LevelInfos &GetBids() const { return bids_; }
+  const LevelInfos &GetAsks() const { return asks_; }
+
+private:
+  LevelInfos bids_;
+  LevelInfos asks_;
+};
+
+class Order {
+public:
+  Order(OrderType orderType, OrderId orderId, Side side, Price price,
+        Quantity quantity)
+      : orderType_{orderType}, orderId_{orderId}, side_{side}, price_{price},
+        initialQuantity_{quantity}, remainingQuantity_{quantity} {}
+
+  OrderId GetOrderId() const { return orderId_; }
+  Side GetSide() const { return side_; }
+  Price GetPrice() const { return price_; }
+  OrderType GetOrderType() const { return orderType_; }
+  Quantity GetInitialQuantity() const { return initialQuantity_; }
+  Quantity GetRemainingQuantity() const { return remainingQuantity_; }
+
+private:
+  OrderType orderType_;
+  OrderId orderId_;
+  Side side_;
+  Price price_;
+  Quantity initialQuantity_;
+  Quantity remainingQuantity_;
+};
+
+int main() { return 0; }
